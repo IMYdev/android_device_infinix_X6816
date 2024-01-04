f7b614faaa2e9151f928af1bbdfe3425268d6621
IMYdev <omranmansour096@gmail.com>
2024-01-04 20:58:10 +0200

    updates

--- device.mk
+++ device.mk
@@ -11,6 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # A/B
+ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 PRODUCT_PACKAGES += \
@@ -18,14 +19,36 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl.recovery \
     android.hardware.boot@1.2-service
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    system \
+    product \
+    system_ext \
+    vendor \
+    vbmeta \
+    vbmeta_system \
+    vbmeta_vendor
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
 PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
 
-PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 # fastbootd
 PRODUCT_PACKAGES += \
