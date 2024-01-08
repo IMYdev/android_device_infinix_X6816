f71dda66fbc333b9e64752484085ff4a57c1fb7e
IMYdev <omranmansour096@gmail.com>
2024-01-08 23:49:48 +0200

    edit .rc files

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,17 +1,17 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := $(call my-dir)
 
-# Recovery init configuration files
+# fstab
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.mt6765.rc
+LOCAL_MODULE	   := fstab.mt6765_ramdisk
+LOCAL_MODULE_STEM  := fstab.mt6765
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.mt6765.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    := etc/fstab.mt6765
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
-
