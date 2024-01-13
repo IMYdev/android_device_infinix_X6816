6546ec8d5dba949fe5842da42327c741ff94d842
IMYdev <omranmansour096@gmail.com>
2024-01-13 17:34:00 +0200

    try fixing the bootloader problem

---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Remove Existing Repos
+#rm -rf system/bpf
+rm -rf hardware/mediatek
+
+# Clone Repos
+#git clone https://github.com/kelexine/bpf system/bpf
+git clone https://github.com/nashc-dev/android_hardware_mediatek hardware/mediatek
+
